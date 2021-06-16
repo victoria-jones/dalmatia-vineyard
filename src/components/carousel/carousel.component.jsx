35fc5694650e57782bd6c3f19675a00c7a5126bc
Victoria Jones <47201480+victoria-jones@users.noreply.github.com>
2021-06-16 15:20:47 -0700

    added carousel component (not yet in use or scripted), added grid layout to hero component

---
+++ src/components/carousel/carousel.component.jsx
@@ -0,0 +1,20 @@
+import React { useEffect, useState } from 'react';
+
+import './carousel.styles.scss';
+import { useEffect } from 'react';
+
+const Carousel = () => {
+
+    const [carouselImages, setCarouselImages] = useState();
+
+    //take images as props
+        //set as background images in inside div
+    //(optional) take children to fill inside carousel. (on top of images)
+
+    return(
+        
+    );
+
+};
+
+export default Carousel;
--- src/components/hero/hero.component.jsx
+++ src/components/hero/hero.component.jsx
@@ -6,17 +6,18 @@ import ImageBoxBordered from '../image-box-bordered/image-box-bordered.component
 import './hero.styles.scss';
 
 import weddingImage from '../../assets/wedding.jpg';
+import eventImage from '../../assets/event-planning.jpg';
 
 const Hero = () => (
     <section className="hero">
         <div className="hero__carousel">
-            <div className="imagebox-placeholder">
+            <div className="carousel__container hero__carousel--container">
                 <div className="hero__header">
                     <h1 className="hero__header--logo logo-font">Dalmatia</h1>
                     <h2 className="hero__header--subhead header-font">Vineyard Estate</h2>
                 </div>
                 <div className="hero__cta">
-                    <span className="cta-font">
+                    <span className="hero__cta--text">
                         host special events and weddings
                     </span>
                     <CustomButton white link="" buttonClass="hero__button">learn more</CustomButton>
@@ -32,9 +33,27 @@ const Hero = () => (
         </div>
 
         <div className="hero__highlight hero__highlight--event-planning">
-            <ImageBoxBordered>
+            <ImageBoxBordered backgroundImage={eventImage}>
                 <CustomButton white link="" buttonClass="hero__button hero__highlight-button">event planning</CustomButton>
             </ImageBoxBordered>
+        </div>
+
+        <div className="hero__carousel-highlights">
+            <div className="hero__carousel-highlights hero__carousel-highlights--1">
+                highlight 1
+            </div>
+
+            <div className="hero__carousel-highlights hero__carousel-highlights--2">
+                highlight 2
+            </div>
+
+            <div className="hero__carousel-highlights hero__carousel-highlights--3">
+                highlight 3
+            </div>
+
+            <div className="hero__carousel-highlights hero__carousel-highlights--4">
+                highlight 4
+            </div>
         </div>
     </section>
 );
